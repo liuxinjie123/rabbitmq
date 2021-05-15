50003c2808a5798ebebcc3aee8b3e8103cf1af96
liuxinjie <jackdream@163.com>
2021-05-15 23:24:45 +0800

    rabbitmq

---
+++ consumerA/src/main/java/com/mq/consumera/controller/DirectReceiver.java
@@ -0,0 +1,15 @@
+package com.mq.consumera.controller;
+
+import com.mq.common.pojo.MQObj;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RabbitListener(queues = "TestDirectQueue")  //监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+    @RabbitHandler
+    public void process(MQObj obj) {
+        System.out.println("DirectReceiver A 消费者收到消息  : " + obj);
+    }
+}
